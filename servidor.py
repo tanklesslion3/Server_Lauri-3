250cde720714ec8de6ea32ed5fca18e2576d4925
tanklesslion3 <66030769+tanklesslion3@users.noreply.github.com>
2024-06-17 21:57:47 +0000

    Mundo guardado 2024-06-17

--- servidor.py
+++ servidor.py
@@ -18,19 +18,6 @@
 # us-cal-1	      Estados unidos (California)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# No toques nada de aquí para abajo, puedes dañarlo
 import requests,os,base64
 if not os.path.exists("./.gitignore"):
 	big = "L1B5dGhvbioNCi93b3JrX2FyZWEqDQovc2Vydmlkb3JfbWluZWNyYWZ0DQovbWluZWNyYWZ0X3NlcnZlcg0KL3NlcnZpZG9yX21pbmVjcmFmdF9vbGQNCi90YWlsc2NhbGUtY3MNCi90aGFub3MNCi9zZXJ2ZXJzDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioubXNwDQoqLm91dHB1dA=="
